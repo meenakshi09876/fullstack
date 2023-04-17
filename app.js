72c7542ae1067911929cd625e921845d53242504
Meenakshi <meenakshi.e@atop.com.tw>
2023-04-17 14:32:31 +0530

    added react frontend version

--- app.js
+++ app.js
@@ -12,30 +12,83 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-requested-With,Content-Type,Accept,Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+//   next();
+// });
+
+// app.use("/api/places", placesRoutes); // => /api/places...
+// app.use("/api/users", usersRoutes);
+
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
+
+// app.use((error, req, res, next) => {
+//   if (req.file) {
+//     fs.unlink(req.file.path, () => {
+//       console.log(error);
+//     });
+//   }
+//   if (res.headerSent) {
+//     return next(error);
+//   }
+//   res.status(error.code || 500);
+//   res.json({ message: error.message || "An unknown error occurred!" });
+// });
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtr4g5q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+// async function connect() {
+//   try {
+//     await mongoose.connect(uri);
+//     console.log("connected to mongodb");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// connect();
+
+// app.listen(5000, () => {
+//   console.log("server started on port 5000");
+// });
+
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use(express.static(path.join("public")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin,X-requested-With,Content-Type,Accept,Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
   next();
 });
 
-app.use("/api/places", placesRoutes); // => /api/places...
+app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
-
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
+  res.sendFile(path.resolve(__dirname, "pulic", "index.html"));
 });
+
+// app.use((req, res, next) => {
+//   const error = new HttpError("Could not find this route.", 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, () => {
-      console.log(error);
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
     });
   }
   if (res.headerSent) {
@@ -44,6 +97,7 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
 });
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtr4g5q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 async function connect() {
